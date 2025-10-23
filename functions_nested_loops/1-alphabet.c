905296e5eab57fe94a3226c29c6004a03c423c25
sabrinasakina <ninasmail@hotmail.fr>
2025-10-23 10:00:47 +0000

    so

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * main - affiche "_putchar" suivi d'un saut de ligne
- *
- * Return: toujours 0
+ * print_alphabet - affiche l'alphabet en minuscules suivi d'un saut de ligne
  */
-int main(void)
+void print_alphabet(void)
 {
-    char str[] = "_putchar";
-    int i;
+	char c = 'a';
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
-
-    _putchar('\n');
-
-    return (0);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
