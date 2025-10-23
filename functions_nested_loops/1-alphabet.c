6d4bec39b1487deee3a670779823562f1b200679
sabrinasakina <ninasmail@hotmail.fr>
2025-10-23 09:55:27 +0000

    so

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - affiche "_putchar" suivi d'un saut de ligne
+ *
+ * Return: toujours 0
+ */
+int main(void)
+{
+    char str[] = "_putchar";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    _putchar('\n');
+
+    return (0);
+}
