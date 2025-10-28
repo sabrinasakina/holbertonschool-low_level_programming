b5fdf046bb3a40e12b3038b935b3e6b430a049cd
sabrinasakina <ninasmail@hotmail.fr>
2025-10-28 09:45:23 +0000

    so

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - affiche les chiffres de 0 à 9 sauf 2 et 4
+ *
+ * Return: rien
+ */
+void print_most_numbers(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++)
+    {
+        if (i != 2 && i != 4)
+            _putchar(i + '0');
+    }
+    _putchar('\n');
+}
+
